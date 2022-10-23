d4300dc53c3c353601e0511d861d2b71624e03d0
Cristian Alejandro Pérez Alvis <crialeperez1835@gmail.com>
2022-10-23 00:55:14 -0500

    V.3 botón de menu con funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 const w = window;
 const $header = document.querySelector("header");
+const $nav = document.querySelector("nav");
+const $iconMenu = document.querySelector(".bx");
 
 w.addEventListener("scroll", () =>{
   $header.classList.toggle("active", (w.scrollY > 10))
+})
+
+
+
+
+document.addEventListener("click", (e) =>{
+  if(e.target.matches(".bx-menu")){
+    e.target.classList.toggle("bx-x");
+    $nav.classList.toggle("active");
+  }
+  if(e.target.matches("a[data-menu]")){
+    $iconMenu.classList.remove("bx-x");
+    $iconMenu.classList.add("bx-menu");
+    $nav.classList.remove("active");
+  }
 })
